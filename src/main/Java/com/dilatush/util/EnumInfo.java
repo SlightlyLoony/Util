d08b3c057c4c1b4bc561b9fdbad4999c51b88db7
Tom Dilatush <tom@dilatush.com>
2019-09-02 09:26:03 -0600

    Docs for EnumIndexer and EnumInfo...

--- src/main/Java/com/dilatush/util/EnumInfo.java
+++ src/main/Java/com/dilatush/util/EnumInfo.java
@@ -14,6 +14,13 @@ public class EnumInfo {
     public final String description;
 
 
+    /**
+     * Creates a new instance of this class with the given index, identifying string, and descriptive string.
+     *
+     * @param _index the index
+     * @param _id the identifying string
+     * @param _description the descriptive string
+     */
     public EnumInfo( final int _index, final String _id, final String _description ) {
         index = _index;
         id = _id;
