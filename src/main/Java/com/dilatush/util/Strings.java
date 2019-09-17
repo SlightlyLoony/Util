84da4e51f04ffd5e90522fb137c7239125178639
Tom Dilatush <tom@dilatush.com>
2019-09-17 09:01:28 -0600

    Added Strings.stripTrailingNewline()...

--- src/main/Java/com/dilatush/util/Strings.java
+++ src/main/Java/com/dilatush/util/Strings.java
@@ -30,4 +30,18 @@ public class Strings {
     public static boolean isEmpty( final String _str ) {
         return (_str == null) || (_str.length() == 0);
     }
+
+
+    /**
+     * Returns the specified string after stripping off any trailing newline character.  Note that if the specified string is {@code null} or empty
+     * (zero length), then the specified string is returned unchanged.
+     *
+     * @param _str the string to strip a trailing newline character from
+     * @return the specified string less any trailing newline
+     */
+    public static String stripTrailingNewline( final String _str ) {
+        if( isEmpty( _str ) || ('\n' != _str.charAt( _str.length() - 1 )) )
+            return _str;
+        return _str.substring( 0, _str.length() - 1 );
+    }
 }
