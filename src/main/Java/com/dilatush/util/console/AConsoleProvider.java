7947b3965b9e2210b2ab6cbc0064b3337a85c299
Tom Dilatush <tom@dilatush.com>
2021-01-23 15:20:39 -0700

    Minor updates to the ConsoleServer; mainly comments and Config validation...

--- src/main/Java/com/dilatush/util/console/AConsoleProvider.java
+++ src/main/Java/com/dilatush/util/console/AConsoleProvider.java
@@ -19,15 +19,21 @@ public abstract class AConsoleProvider implements ConsoleProvider {
 
     private Socket socket;
 
-    private BufferedReader reader;
     private BufferedWriter writer;
 
 
+    /**
+     * Create a new instance of this class with the given socket, reader, and writer.  The reader is based on a decrypting stream, already
+     * initialized.  The writer is based on an encrypting stream, also already initialized.
+     *
+     * @param _socket The socket for the network connection to the console client.
+     * @param _reader The reader to read text from the console client.
+     * @param _writer The writer to write text to the console client.
+     */
     @Override
     public void run( final Socket _socket, final BufferedReader _reader, final BufferedWriter _writer ) {
 
         socket = _socket;
-        reader = _reader;
         writer = _writer;
 
         // initialize the provider...
@@ -36,7 +42,7 @@ public abstract class AConsoleProvider implements ConsoleProvider {
         // read input from the network...
         try {
             while( !Thread.currentThread().isInterrupted() ) {
-                String line = reader.readLine();
+                String line = _reader.readLine();
                 if( isNull( line ) )
                     break;
                 onLine( line );
@@ -44,18 +50,29 @@ public abstract class AConsoleProvider implements ConsoleProvider {
         }
         catch( IOException _e ) {
 
-            // close the socket, which will provoke all sorts of hate and discontent...
+            // close the socket, which will provoke all sorts of hate and discontent on both ends of the connection...
             close( socket );
         }
     }
 
 
+    /**
+     * Write the given signoff message to the console client, then close the connection to the client.  This should be called by concrete subclasses
+     * to gracefully terminate the connection.
+     *
+     * @param _signoffMsg The signoff message to send to the console client before closing the connection.
+     */
     protected void exit( final String _signoffMsg ) {
         writeLine( _signoffMsg );
         close( socket );
     }
 
 
+    /**
+     * Write the given string to the console client, followed by a newline, the entirety to be flushed for immediate transmission.
+     *
+     * @param _line The string to send to the console client.
+     */
     protected void writeLine( final String _line ) {
 
         try {
@@ -71,9 +88,26 @@ public abstract class AConsoleProvider implements ConsoleProvider {
     }
 
 
+    /**
+     * Returns the name of this console provider; it must be implemented by concrete subclasses.
+     *
+     * @return the name of this console provider
+     */
     protected abstract String getName();
 
+
+    /**
+     * Called upon receipt of a line of text from the console client; must be implemented by concrete subclasses to process text from the console
+     * client.
+     *
+     * @param _line The line of text received from the console client.
+     */
     protected abstract void onLine( final String _line );
 
+
+    /**
+     * Called when the provider is being started; must be implemented by concrete subclasses to do whatever initialization they may need.
+     * Conventionally this initialization includes sending a signon banner to the console client.
+     */
     protected abstract void init();
 }
--- src/main/Java/com/dilatush/util/console/ConsoleServer.java
+++ src/main/Java/com/dilatush/util/console/ConsoleServer.java
@@ -1,15 +1,20 @@
 package com.dilatush.util.console;
 
 import com.dilatush.util.AConfig;
+import com.dilatush.util.Base64;
 import com.dilatush.util.Networking;
 
 import java.io.IOException;
+import java.lang.reflect.Constructor;
 import java.net.*;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import static com.dilatush.util.General.isNull;
+import static com.dilatush.util.Strings.isEmpty;
 
 /**
  * Implements an embeddable TCP console server, designed to let embedded or daemon applications provide a simple command line interface.
@@ -155,7 +160,74 @@ public class ConsoleServer {
          */
         @Override
         public void verify( final List<String> _messages ) {
+            validate( () -> maxClients > 0, _messages, "maxClients must be greater than zero" );
+            validate( () -> !isEmpty( name ), _messages, "name must be present and not empty" );
+            validate( this::validateKey, _messages, "key must be 22 characters of valid base64" );
+            validate( () -> (port >= 1024) && (port <= 65535), _messages, "port must be in the range [1024..65535" );
+            validateProviders( _messages );
+        }
 
+
+        private void validateProviders( final List<String> _messages ) {
+
+            // if we have no map, we're most definitely invalid...
+            if( isNull( providers ) ) {
+                validate( () -> false, _messages, "providers must be initialized" );
+                return;
+            }
+
+            // iterate over all the configured providers...
+            for( Map.Entry<String,String> provider : providers.entrySet() ) {
+
+                // if the name is empty, problem...
+                if( isEmpty( provider.getKey() ) ) {
+                    validate( () -> false, _messages, "name of provider may not be the empty string" );
+                }
+
+                // if the configured class does not exist and is not the right type, problem...
+                String klassName = null;
+                try {
+                    klassName = provider.getValue();
+                    Class<?> klass = Class.forName( klassName );
+
+                    // make sure it implements ConsoleProvider...
+                    if( !ConsoleProvider.class.isAssignableFrom( klass ) )
+                        validate( () -> false, _messages, "class " + klassName + " is not a console provider" );
+
+                    // get the no-args constructor for the console provider class...
+                    Constructor<?> ctor = klass.getConstructor();
+                }
+                    catch( ClassNotFoundException _e ) {
+                        validate( () -> false, _messages, "class " + klassName + " does not exist" );
+                }
+                catch( NoSuchMethodException _e ) {
+                    validate( () -> false, _messages, "class " + klassName + " does not have a no-args constructor" );
+                }
+            }
+        }
+
+
+        /**
+         * Return {@code true} if the key (a string) is valid base64 that decodes to 16 bytes.
+         *
+         * @return {@code true} if the key (a string) is valid base64 that decodes to 16 bytes
+         */
+        private boolean validateKey() {
+
+            // if we have no string, we're definitely not valid...
+            if( isEmpty( key ) )
+                return false;
+
+            // see if the string decodes to 16 bytes; if so, we're golden, otherwise, not so much...
+            try {
+                byte[] test = Base64.decodeBytes( key );
+                return test.length == 16;
+            }
+
+            // we'll get here if the string is not valid base64...
+            catch( Exception _e ) {
+                return false;
+            }
         }
     }
 }
