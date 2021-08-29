e2905c7f8a78018ed9fa7de957f1bfecf3853964
tom <tom@dilatush.com>
2021-08-29 12:21:02 -0600

    Comments and toPrint() in DNSQuestion...

--- src/main/Java/com/dilatush/util/dns/DNSQuestion.java
+++ src/main/Java/com/dilatush/util/dns/DNSQuestion.java
@@ -11,10 +11,9 @@ import java.util.Map;
 
 import static com.dilatush.util.General.isNull;
 
-// TODO: add toString()...
-// TODO: add comments...
 /**
- * Instances of this class represent a DNS question.  Instances are immutable and threadsafe.
+ * Instances of this class represent a DNS question, with a domain name (the subject of the question), the resource record type desired, and the
+ * resource record.  Instances are immutable and threadsafe.
  *
  * @author Tom Dilatush  tom@dilatush.com
  */
@@ -24,14 +23,26 @@ public class DNSQuestion {
     private static final Outcome.Forge<DNSQuestion> outcome       = new Outcome.Forge<>();
     private static final Outcome.Forge<?>           encodeOutcome = new Outcome.Forge<>();
 
+    /** The domain name to ask a question about. */
     public final DNSDomainName qname;
+
+    /** The type of resource records wanted that pertain to the queried domain name. */
     public final DNSRRType     qtype;
+
+    /** The class of resource records wanted that pertain to the queried domain name. */
     public final DNSRRClass    qclass;
 
 
+    /**
+     * Creates a new instance of this class.  Note that this is a private constructor, used only by factory methods and decoders.
+     *
+     * @param _qname  The domain name to ask a question about.
+     * @param _qtype  The type of resource records wanted that pertain to the queried domain name.
+     * @param _qclass  The class of resource records wanted that pertain to the queried domain name.
+     */
     private DNSQuestion( final DNSDomainName _qname, final DNSRRType _qtype, final DNSRRClass _qclass ) {
-        qname = _qname;
-        qtype = _qtype;
+        qname  = _qname;
+        qtype  = _qtype;
         qclass = _qclass;
     }
 
@@ -61,6 +72,25 @@ public class DNSQuestion {
     }
 
 
+    /**
+     * Return a string representing this instance.
+     *
+     * @return a string representing this instance.
+     */
+    public String toString() {
+        return "DNS Question: " + qname.text + ", type: " + qtype.text + ", class: " + qclass.text;
+    }
+
+
+    /**
+     * Attempts to create a new instance of this class with the given parameters.  If successful, an ok outcome containing the new instance is
+     * returned.  Otherwise, a not ok outcome is returned with a message explaining the problem.
+     *
+     * @param _qname  The domain name to ask a question about.
+     * @param _qtype  The type of resource records wanted that pertain to the queried domain name.
+     * @param _qclass  The class of resource records wanted that pertain to the queried domain name.
+     * @return The {@link Outcome Outcome&lt;DNSQuestion&gt;} with the results of the attempt.
+     */
     public static Outcome<DNSQuestion> create(  final DNSDomainName _qname, final DNSRRType _qtype, final DNSRRClass _qclass ) {
 
         if( isNull( _qname, _qtype, _qclass ) )
@@ -70,30 +100,44 @@ public class DNSQuestion {
     }
 
 
+    /**
+     * Attempts to create a new instance of this class with the given parameters and a resource record class of {@link DNSRRClass#IN} (Internet class
+     * records, which are nearly always what is desired).  If successful, an ok outcome containing the new instance is
+     * returned.  Otherwise, a not ok outcome is returned with a message explaining the problem.
+     *
+     * @param _qname  The domain name to ask a question about.
+     * @param _qtype  The type of resource records wanted that pertain to the queried domain name.
+     * @return The {@link Outcome Outcome&lt;DNSQuestion&gt;} with the results of the attempt.
+     */
     public static Outcome<DNSQuestion> create(  final DNSDomainName _qname, final DNSRRType _qtype ) {
         return create( _qname, _qtype, DNSRRClass.IN );
     }
 
 
-
-        /**
-         * Attempts to create a new instance of this class from the bytes at the current position of the given {@link ByteBuffer}.
-         *
-         * @param _buffer The {@link ByteBuffer} containing the bytes encoding the question.
-         * @return The {@link Outcome Outcome&lt;DNSQuestion&gt;} giving the results of the attempt.
-         */
+    /**
+     * Attempts to create a new instance of this class from the bytes at the current position of the given {@link ByteBuffer}.
+     *
+     * @param _buffer The {@link ByteBuffer} containing the bytes encoding the question.
+     * @return The {@link Outcome Outcome&lt;DNSQuestion&gt;} giving the results of the attempt.
+     */
     public static Outcome<DNSQuestion> decode( final ByteBuffer _buffer ) {
 
+        // decode the domain name...
         Outcome<DNSDomainName> domainNameOutcome = DNSDomainName.decode( _buffer );
         if( !domainNameOutcome.ok() )
             return outcome.notOk( "Could not decode qname: " + domainNameOutcome.msg() );
+
+        // decode the resource record type...
         Outcome<DNSRRType> rrTypeOutcome = DNSRRType.decode( _buffer );
         if( !rrTypeOutcome.ok() )
             return outcome.notOk( "Could not decode qtype: " + rrTypeOutcome.msg() );
+
+        // decode the resource record class...
         Outcome<DNSRRClass> rrClassOutcome = DNSRRClass.decode( _buffer );
         if( !rrClassOutcome.ok() )
             return outcome.notOk( "Could not decode qclass: " + rrClassOutcome.msg() );
 
+        // if we get here we decoded everything, so leave with our shiny newly decoded instance...
         return outcome.ok( new DNSQuestion( domainNameOutcome.info(), rrTypeOutcome.info(), rrClassOutcome.info() ) );
     }
 }
