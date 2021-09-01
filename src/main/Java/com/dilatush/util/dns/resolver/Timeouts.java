59020b25f567fd31245276c87eb3a4c460f75e30
tom <tom@dilatush.com>
2021-09-01 09:12:24 -0600

    Updated Timeouts and Timeout classes...

--- src/main/Java/com/dilatush/util/dns/resolver/Timeouts.java
+++ src/main/Java/com/dilatush/util/dns/resolver/Timeouts.java
@@ -3,34 +3,50 @@ package com.dilatush.util.dns.resolver;
 import java.util.ArrayList;
 import java.util.List;
 
-import static java.lang.Thread.sleep;
-
 /**
- * Instances of this class efficiently manage a collection of timeouts.  Methods allow for adding new timeouts, removing timeouts, cancelling
- * timeouts, and checking timeouts to see if they've actually timed out.
+ * Instances of this class efficiently manage a collection of timeouts.  Methods allow for adding new timeouts and checking timeouts to see if they've
+ * actually timed out.
  */
 public class Timeouts {
 
+    // The list of active timeouts, in order of their expiration times (earlier times first)...
     private final List<Timeout> timeouts = new ArrayList<>();
 
 
     public synchronized void add( final Timeout _newTimeout ) {
+
+        // Walk backwards down the list, looking for a timeout expiring no later than the new timeout; we want to insert the
+        // new one right after that.  We walk backwards on the theory that if the list is large, the insertion point will be closer to the end than
+        // to the beginning of the list...
         for( int i = timeouts.size() - 1; i >= 0; i-- ) {
-            if( _newTimeout.getExpiration() <= timeouts.get( i ).getExpiration() ) {
+
+            // if the timeout from the list expires no later than the new timeout...
+            if( timeouts.get( i ).getExpiration() <= _newTimeout.getExpiration() ) {
+
+                // insert the new one right after it, and we're done...
                 timeouts.add( i + 1, _newTimeout );
                 return;
             }
         }
-        timeouts.add( _newTimeout );
+
+        // if the list was empty, or all the timeouts in it expire after the new one, then the new one needs to be the first in the list...
+        timeouts.add( 0, _newTimeout );
     }
 
 
+    /**
+     * Check to see if any of the timeouts have expired, and if so, handle them.
+     */
     public synchronized void check() {
+
+        // walk the list of timeouts until we run into one that has not expired yet...
         int i;
         for( i = 0; i < timeouts.size(); i++ ) {
             if( !timeouts.get( i ).hasExpired() )
                 break;
         }
+
+        // clear all the expired timeouts off the beginning of the list...
         timeouts.subList( 0, i ).clear();
     }
 }
