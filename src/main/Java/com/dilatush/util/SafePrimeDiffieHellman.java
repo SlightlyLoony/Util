9fd13a2a90ceaefe9cbef86166e766b8b7523972
tom <tom@dilatush.com>
2022-07-19 14:49:59 -0600

    First cut at an extended Diffie-Hellman implementation.

--- src/main/Java/com/dilatush/util/SafePrimeDiffieHellman.java
+++ src/main/Java/com/dilatush/util/SafePrimeDiffieHellman.java
@@ -7,6 +7,7 @@ import java.security.SecureRandom;
 
 import static com.dilatush.util.General.isNotNull;
 import static com.dilatush.util.General.isNull;
+import static java.math.BigInteger.ONE;
 
 /**
  * <p>Provides an implementation of the Diffie-Hellman key exchange protocol, where the p and g parameters have been generated using the procedure given in "Cryptography
@@ -14,7 +15,8 @@ import static com.dilatush.util.General.isNull;
  * checked key exchange.</p>
  * <p>The key exchange protocol has two "sides".  Usually that means two computers connected by a network, but it could be any two processes that need a shared
  * secret.  For the discussion below, we use "this side" to refer to the process using an instance of this class, and "the other side" to refer to another
- * process implementing the same protocol, possibly with another instance of this class.  To use this class, follow these steps in order:</p>
+ * process implementing the same protocol, possibly with another instance of this class.  To use this class as a standard Diffie-Hellman (but using safe primes),
+ * follow these steps in order:</p>
  * <ol>
  *     <li>Create a new instance of this class for this side.</li>
  *     <li>Set the Diffie-Hellman parameters (p, g, and bit length) for this side using the {@link #setParameters(DHParameterSpec)} method.  Note that both this side
@@ -24,6 +26,26 @@ import static com.dilatush.util.General.isNull;
  *     <li>Get the other side's public key and set it in this instance, using the {@link #setOtherSidePublicKey(BigInteger)} method.</li>
  *     <li>Get the shared secret, using the {@link #getSharedSecret()} method.</li>
  * </ol>
+ * <p>The standard Diffie-Hellman implementation, as above, has a problem: it's vulnerable to a man-in-the-middle (MITM) attack (See page 183 of Cryptography Engineering).
+ * There are many ways to mitigate this problem.  Many of these ways boil down to having a permanently shared "identity secret" that is known to both sides, but not to
+ * the MITM.  This class extends Diffie-Hellman slightly to add the notion of the identity secret.  This addition prevents the MITM from knowing the secret shared by the two
+ * ends of the Diffie-Hellman exchange.  Since that shared secret is usually used to create a shared symmetric encryption key, that means the MITM will not be able to decrypt
+ * the traffic encrypted with the shared symmetric encryption key.</p>
+ * <p>The standard Diffie-Hellman implementation computes the shared secret as (other side's public key)^(this side's private key) modulo p.  We'll call that value "ss".  The
+ * extension computes the shared secret as ss^(identity secret) modulo p.  This is the same as g^((other side's private key)*(this side's private key)*(identity secret) modulo
+ * p, and the resulting value is in the same range [1..p-1] as with the standard Diffie-Hellman.  To use this class with the identity secret extension to standard Diffie-
+ * Hellman, using safe primes, follow these steps in order:</p>
+ * <ol>
+ *     <li>Create a new instance of this class for this side.</li>
+ *     <li>Set the Diffie-Hellman parameters (p, g, and bit length) for this side using the {@link #setParameters(DHParameterSpec)} method.  Note that both this side
+ *     and the other side must use identical Diffie-Hellman parameters.</li>
+ *     <li>Set the identity secret using the {@link #setIdentitySecret(BigInteger)} method.  Note that this side and the other side must have identical identity secrets.</li>
+ *     <li>Generate the private and public keys for this side using the {@link #generateKeys()} method.  The generated keys are stored in this instance.</li>
+ *     <li>Get the public key (using the {@link #getPublicKey()} method) and send it to the other side.</li>
+ *     <li>Get the other side's public key and set it in this instance, using the {@link #setOtherSidePublicKey(BigInteger)} method.</li>
+ *     <li>Get the shared secret, using the {@link #getSharedSecret()} method.</li>
+ * </ol>
+ * <p>Only the third step is different -- setting the identity secret causes it to be included in the shared secret calculation.</p>
  * <p>Instances of this class are mutable and <i>not</i> threadsafe.</p>
  */
 public class SafePrimeDiffieHellman {
@@ -73,6 +95,11 @@ public class SafePrimeDiffieHellman {
      * The public key from the other side.
      */
     private BigInteger otherSidePublicKey;
+
+    /**
+     * The optional identity secret.
+     */
+    private BigInteger identitySecret;
 
     /**
      * The computed shared secret.
@@ -143,9 +170,9 @@ public class SafePrimeDiffieHellman {
 
         // make sure that g is less than p, is positive, and is a square modulo p...
         BigInteger tg = _spec.getG();
-        if( tg.compareTo( tp.subtract( BigInteger.ONE ) ) >= 0 )
+        if( tg.compareTo( tp.subtract( ONE ) ) >= 0 )
             return forgeObject.notOk( "g is not smaller than p - 1" );
-        if( tg.compareTo( BigInteger.ONE ) <= 0 )
+        if( tg.compareTo( ONE ) <= 0 )
             return forgeObject.notOk( "g is not larger than one" );
         if( Crypto.legendreSymbol( tg, tp ) != 1 )
             return forgeObject.notOk( "g modulo p is not a square" );
@@ -179,7 +206,7 @@ public class SafePrimeDiffieHellman {
         // all is well, so let's pick a random private key in the range [1..p-1]...
         do {
             privateKey = new BigInteger( l, secureRandom );
-        } while( (privateKey.compareTo( BigInteger.ONE ) < 0) || (privateKey.compareTo( p ) >= 0) );
+        } while( (privateKey.compareTo( ONE ) < 0) || (privateKey.compareTo( p ) >= 0) );
 
         // and then compute the public key...
         publicKey = g.modPow( privateKey, p );
@@ -215,19 +242,103 @@ public class SafePrimeDiffieHellman {
      * </ol>
      * <p></p>
      *
-     * @param _publicKey
-     * @return
+     * @param _publicKey The public key received from the other side.
+     * @return The outcome of the operation, "ok" if the other side's public key is acceptable, and "not ok" (with an explanatory message) if it was not.
      */
     public Outcome<Object> setOtherSidePublicKey( final BigInteger _publicKey ) {
 
-        return null;
+        // make sure we haven't already set the public key...
+        if( isNotNull( otherSidePublicKey ) )
+            return forgeObject.notOk( "public key has already been set" );
 
+        // make sure the parameter is non-null...
+        if( isNull( _publicKey ) )
+            return forgeObject.notOk( "_publicKey is null" );
+
+        // make sure the given public key is in the range [1..p-1]...
+        if( (_publicKey.compareTo( ONE ) < 0) || (_publicKey.compareTo( p ) >= 0) )
+            return forgeObject.notOk( "_publicKey is less than 1 or greater than p - 1" );
+
+        // make sure the given public key is a square modulo p...
+        if( Crypto.legendreSymbol( _publicKey, p ) != 1 )
+            return forgeObject.notOk( "_publicKey is not a square modulo p" );
+
+        // if we get here, we passed all the tests and we can set the public key...
+        otherSidePublicKey = _publicKey;
+
+        return forgeObject.ok();
     }
 
 
+    /**
+     * Sets the identity secret, which will then be incorporated into the shared secret computation.  The identity secret must be a positive integer with at least 128 bits.
+     *
+     * @param _identitySecret The identity secret shared with the other side.
+     * @return The outcome of this operation, "ok" if the identity secret was set successfully, and "not ok" (with an explanatory message) if it was not.
+     */
+    public Outcome<Object> setIdentitySecret( final BigInteger _identitySecret ) {
+
+        // make sure the identity secret has not already been set...
+        if( isNotNull( identitySecret ) )
+            return forgeObject.notOk( "identity secret has already been set" );
+
+        // make sure the parameters have been set...
+        if( isNull( p, g ) || (l == 0) )
+            return forgeObject.notOk( "parameters have not been set" );
+
+        // make sure that keys have not been generated...
+        if( isNotNull( privateKey ) )
+            return forgeObject.notOk( "keys have already been generated" );
+
+        // make sure we actually got an identity secret...
+        if( isNull( _identitySecret ) )
+            return forgeObject.notOk( "_identitySecret is null" );
+
+        // make sure we have at least 128 bits in our identity secret...
+        if( _identitySecret.bitLength() < 128 )
+            return forgeObject.notOk( "_identitySecret does not have at least 128 bits" );
+
+        // if we make it here, then our identity secret is good to go...
+        identitySecret = _identitySecret;
+        return forgeObject.ok();
+    }
+
+
+    /**
+     * Returns the computed shared secret (if it has not already been computed).  This method will fail if this instance does not have a private key (because
+     * keys have been successfully generated) or if the other side's public key has not been set.
+     *
+     * @return The outcome of the operation, "ok" with the shared secret if the operation is successful, and "not ok" (with an explanatory message) if it was
+     *         not.
+     */
     public Outcome<BigInteger> getSharedSecret() {
 
-        return null;
+        // if we've already computed the shared secret, return it with an ok...
+        if( isNotNull( sharedSecret ) )
+            return forgeBigInteger.ok( sharedSecret );
+
+        // if we get here, we need to compute the shared secret, making certain we have the requisite information...
+
+        // make sure we have a private key...
+        if( isNull( privateKey ) )
+            return forgeBigInteger.notOk( "private key has not been generated" );
+
+        // make sure we have the other side's public key...
+        if( isNull( otherSidePublicKey ) )
+            return forgeBigInteger.notOk( "other side's public key has not been set" );
+
+        // if we get here, we're all ready to calculate our shared secret...
+
+        // first we compute the standard Diffie-Hellman shared secret...
+        BigInteger ss = otherSidePublicKey.modPow( privateKey, p );
+
+        // if we have an identity secret, add its computation...
+        if( isNotNull( identitySecret ) )
+            ss = ss.modPow( identitySecret, p );
+
+        // store the result away and return it...
+        sharedSecret = ss;
+        return forgeBigInteger.ok( sharedSecret );
 
     }
 
@@ -237,11 +348,14 @@ public class SafePrimeDiffieHellman {
         // some setup...
         SecureRandom secureRandom = new SecureRandom();
         DHParameterSpec dhps = Crypto.getDHParameters( 512, secureRandom );
+        BigInteger is = new BigInteger( 256, secureRandom );
 
         // this side...
         SafePrimeDiffieHellman tsspdh = new SafePrimeDiffieHellman( secureRandom );
         Outcome<Object> setParametersOutcome = tsspdh.setParameters( dhps );
         if( setParametersOutcome.notOk()) throw new IllegalStateException( "this side setParameter: " + setParametersOutcome.msg() );
+        Outcome<Object> tssis = tsspdh.setIdentitySecret( is );
+        if( tssis.notOk() ) throw new IllegalStateException( "this side setIdentitySecret: " + tssis.msg() );
         Outcome<Object> generateOutcome = tsspdh.generateKeys();
         if( generateOutcome.notOk() ) throw new IllegalStateException( "this side generateKeys: " + generateOutcome.msg() );
         Outcome<BigInteger> thisSideGetPublicKeyOutcome = tsspdh.getPublicKey();
@@ -252,6 +366,8 @@ public class SafePrimeDiffieHellman {
         SafePrimeDiffieHellman osspdh = new SafePrimeDiffieHellman( secureRandom );
         setParametersOutcome = osspdh.setParameters( dhps );
         if( setParametersOutcome.notOk()) throw new IllegalStateException( "other side setParameter: " + setParametersOutcome.msg() );
+        Outcome<Object> ossis = osspdh.setIdentitySecret( is );
+        if( ossis.notOk() ) throw new IllegalStateException( "other side setIdentitySecret: " + ossis.msg() );
         generateOutcome = osspdh.generateKeys();
         if( generateOutcome.notOk() ) throw new IllegalStateException( "other side generateKeys: " + generateOutcome.msg() );
         Outcome<BigInteger> otherSideGetPublicKeyOutcome = osspdh.getPublicKey();
@@ -259,7 +375,18 @@ public class SafePrimeDiffieHellman {
         BigInteger otherSidePublicKey = otherSideGetPublicKeyOutcome.info();
 
         // this side...
+        Outcome<Object> tssospko = tsspdh.setOtherSidePublicKey( otherSidePublicKey );
+        if( tssospko.notOk() ) throw new IllegalStateException( "other side public key: " + tssospko.msg() );
+        Outcome<BigInteger> tsgsso = tsspdh.getSharedSecret();
+        if( tsgsso.notOk() ) throw new IllegalStateException( "this side get shared secret: " + tsgsso );
+        BigInteger tsss = tsgsso.info();
 
+        // other side...
+        Outcome<Object> ossospko = osspdh.setOtherSidePublicKey( thisSidePublicKey );
+        if( ossospko.notOk() ) throw new IllegalStateException( "this side public key: " + ossospko.msg() );
+        Outcome<BigInteger> osgsso = osspdh.getSharedSecret();
+        if( osgsso.notOk() ) throw new IllegalStateException( "other side get shared secret: " + osgsso );
+        BigInteger osss = osgsso.info();
 
         dhps.hashCode();
     }
